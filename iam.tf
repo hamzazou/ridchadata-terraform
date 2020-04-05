c06677161bf3db76bf6abf8fe7a1c0cd7efe0c80
hamza <hbensalem@gmail.com>
2020-04-05 07:31:52 -0700

    add necessery users and groups

--- iam.tf
+++ iam.tf
@@ -1,7 +1,44 @@
 provider "aws" {
 }
 
-resource "aws_iam_group" "developers" {
-  name = "developers"
+resource "aws_iam_group" "groups" {
+  count = "${length(var.groupname)}"
+  name = "${element(var.groupname,count.index )}"
   path = "/aisec/"
+}
+
+resource "aws_iam_user" "dev" {
+  count = "${length(var.usernamedev)}"
+  name = "${element(var.usernamedev,count.index )}"
+}
+
+resource "aws_iam_user" "infra" {
+  count = "${length(var.usernameinfra)}"
+  name = "${element(var.usernameinfra,count.index )}"
+}
+
+resource "aws_iam_user" "test" {
+  count = "${length(var.usernametest)}"
+  name = "${element(var.usernametest,count.index )}"
+}
+
+resource "aws_iam_group_membership" "team-dev" {
+  name = "team-dev"
+  users = var.usernamedev
+
+  group = "dev-aisec"
+}
+
+resource "aws_iam_group_membership" "team-infra" {
+  name = "team-infra"
+  users = var.usernameinfra
+
+  group = "infra-aisec"
+}
+
+resource "aws_iam_group_membership" "team-test" {
+  name = "team-test"
+  users = var.usernametest
+
+  group = "test-aisec"
 }
--- variable.tf
+++ variable.tf
@@ -2,3 +2,24 @@ variable "region" {
   default = "us-west-2"
   type = "string"
 }
+
+variable "groupname" {
+  type = "list"
+  default = ["dev-aisec","infra-aisec","test-aisec"]
+}
+variable "usernamedev" {
+  type = "list"
+  default = ["dev1","dev2","dev3"]
+}
+
+variable "usernameinfra" {
+  type = "list"
+  default = ["infra1","infra2","infra3"]
+}
+
+variable "usernametest" {
+  type = "list"
+  default = ["test1","test2","test3"]
+}
+
+
